1f4b81500ccd909e28dc903f805d86864f8fc573
Alexander Weld/Luzon <alex.weld@gmx.net>
2012-11-01 11:07:12 +0100

    data und zeug.

---
+++ data/bundeslaender2009.js
@@ -0,0 +1,23 @@
+﻿bundeslaender2009_data = [
+                {
+                    "id": "1",
+					"parentid": "-1",
+                    "text": "Bayern"
+                }, {
+                    "id": "2",
+					"parentid": "-1",
+                    "text": "BaWü"
+                }, {
+                    "id": "3",
+					"parentid": "-1",
+                    "text": "Berlin"
+                }, {
+                    "id": "4",
+					"parentid": "-1",
+                    "text": "Bremen"
+                }, {
+                    "id": "5",
+					"parentid": "-1",
+                    "text": "..."
+                }
+			]
