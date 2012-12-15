a250e173aa9e08d53956cb9e478a21adfe542d0a
kawuum <siegfried.depner@googlemail.com>
2012-12-15 15:59:32 +0100

    header ausgelagert...

--- ergebnisse_bundesebene.php
+++ ergebnisse_bundesebene.php
@@ -1,21 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta name="keywords" content="jQuery Menu, Main Menu, Context Menu, Vertical Menu, Popup Menu, Menu, jqxMenu" /> 
-    <meta name="description" content="The jqxMenu widget supports several built-in modes - horizontal, vertical and context menu. In this demo, the mode property is set to 'vertical'."/>
-    <title id='Description'>The jqxMenu widget supports several built-in modes - horizontal, vertical and context menu. In this demo, the mode property is set to 'vertical'.
-    </title>    	
-		
-	<link rel="stylesheet" href="lib/jqwidgets/styles/jqx.base.css" type="text/css" />
-    <script type="text/javascript" src="lib/scripts/gettheme.js"></script>
-    <script type="text/javascript" src="lib/scripts/jquery-1.8.2.min.js"></script>
-    <script type="text/javascript" src="lib/jqwidgets/jqxcore.js"></script>
-	<script type="text/javascript" src="lib/jqwidgets/jqxdata.js"></script>
-	<script type="text/javascript" src="lib/jqwidgets/jqxchart.js"></script>
-	<script type="text/javascript" src="lib/jqwidgets/jqxmenu.js"></script>
-	<script type="text/javascript" src="lib/jqwidgets/jqxdockpanel.js"></script>
-	
-</head>
+<?php include("head.php");?>
 <body>
 
 	<div id='jqxDockPanel' style="background: none; border: none;">	
